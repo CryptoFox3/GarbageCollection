3398a95637444a7297c79e83b29d23202669643b
cryptofox3 <pghodgson558@gmail.com>
2018-07-26 13:38:12 -0500

    Added 'username' field in Register.cshtml file

--- GarbageCollection/Models/AccountViewModels.cs
+++ GarbageCollection/Models/AccountViewModels.cs
@@ -53,6 +53,8 @@ namespace GarbageCollection.Models
         [EmailAddress]
         public string Email { get; set; }
 
+        
+
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
@@ -68,6 +70,11 @@ namespace GarbageCollection.Models
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "User Name")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        public string Username { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
